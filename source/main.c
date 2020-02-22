62a39a75505058995df67e80110d768d65340d83
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2020-02-22 10:47:18 +0900

    main.c, configure{,.ac}: bump version v0.54

--- source/main.c
+++ source/main.c
@@ -19,7 +19,7 @@ char *styfile,*idxfile[256],*indfile,*dicfile,*logfile;
 #endif
 KpathseaSupportInfo kp_ist,kp_dict;
 
-#define VERSION "version 0.53"
+#define VERSION "version 0.54"
 
 int main(int argc, char **argv)
 {
