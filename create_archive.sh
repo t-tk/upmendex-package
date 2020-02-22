5abc72ec4723ee9c585b4ecd0a29861bf6dbdff6
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2020-02-22 14:06:49 +0900

    create_archive.sh: remove *.tds.zip

--- create_archive.sh
+++ create_archive.sh
@@ -10,41 +10,6 @@ if [ -z "$RELEASEDATE" ]; then
     RELEASEDATE="**not tagged**; later than $LATESTRELEASEDATE?"
 fi
 
-echo " * Create $PROJECT.tds.zip"
-rm -rf $TMP/$PROJECT/*
-git archive --format=tar --prefix=$PROJECT/ HEAD | (cd $TMP && tar xf -)
-rm $TMP/$PROJECT/create_archive.sh
-#perl -pi.bak -e "s/\\\$RELEASEDATE/$RELEASEDATE/g" $TMP/$PROJECT/README.md
-#rm -f $TMP/$PROJECT/README.md.bak
-
-SRC_DIR=$TMP/$PROJECT/Build/source/texk/$PROJECT
-DOC_DIR=$TMP/$PROJECT/Master/texmf-dist/doc/$PROJECT
-MAN_DIR=$TMP/$PROJECT/Master/texmf-dist/doc/man/man1
-
-mkdir -p $SRC_DIR
-mv $TMP/$PROJECT/source/* $SRC_DIR/
-rmdir $TMP/$PROJECT/source
-
-mkdir -p $DOC_DIR
-mv $TMP/$PROJECT/README.md $DOC_DIR/
-mv $TMP/$PROJECT/COPYRIGHT $DOC_DIR/
-mv $TMP/$PROJECT/doc/upmendex.ja.txt $DOC_DIR/
-
-### move doc/samples/* to doc/$PROJECT/samples/*
-mv $TMP/$PROJECT/doc/samples $DOC_DIR/
-rmdir $TMP/$PROJECT/doc
-
-mkdir -p $MAN_DIR
-mv $TMP/$PROJECT/man/upmendex.1 $MAN_DIR/
-mv $TMP/$PROJECT/man/upmendex.man1.pdf $MAN_DIR/
-rm $TMP/$PROJECT/man/upmendex.man1.ps
-rmdir $TMP/$PROJECT/man
-
-cd $TMP/$PROJECT && zip -r $TMP/$PROJECT.tds.zip *
-cd $PWDF
-
-rm -rf $TMP/$PROJECT
-
 echo
 echo " * Create $PROJECT.zip ($RELEASEDATE)"
 git archive --format=tar --prefix=$PROJECT/ HEAD | (cd $TMP && tar xf -)
@@ -55,7 +20,7 @@ rm $TMP/$PROJECT/create_archive.sh
 rm $TMP/$PROJECT/man/upmendex.man1.ps
 
 rm -f $PWDF/$PROJECT.zip
-cd $TMP && zip -r $PWDF/$PROJECT.zip $PROJECT $PROJECT.tds.zip
-rm -rf $TMP/$PROJECT $TMP/$PROJECT.tds.zip
+cd $TMP && zip -r $PWDF/$PROJECT.zip $PROJECT
+rm -rf $TMP/$PROJECT
 echo
 echo " * Done: $PROJECT.zip ($RELEASEDATE)"
